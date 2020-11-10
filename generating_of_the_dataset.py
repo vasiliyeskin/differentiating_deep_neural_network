067aa93bf7aecbef8ab7e699a99c81488e3447c4
vasiliyeskin <vasiliyeskin@yandex.ru>
2020-11-10 21:40:30 +0300

    Add templates of formulas and change the forming data set

--- generating_of_the_dataset.py
+++ generating_of_the_dataset.py
@@ -14,6 +14,8 @@ with open('alphabet.txt', 'r') as file:
 with open('numbers.txt', 'r') as file:
     numbers = [line.strip() for line in file.readlines()]
 
+dict_template_formulas = csv.DictReader(open('template_formulas.txt', newline=''))
+
 dict_writer = csv.DictWriter(open("differentiating_expressions.txt",'w'), fieldnames=fields)
 dict_writer.writeheader()
 
@@ -22,19 +24,18 @@ alp_numbers = alphabet + numbers
 
 len_alphabet = len(alphabet)
 len_alp_Numb = len(alp_numbers)
-for i in range(len_alphabet):
-    data['variable'] = alphabet[i]
-    n = random.randint(0, len_alp_Numb - 1)
-    n = n - 1 if n == i else n
-    data['expression'] = '{0} ^ {{ {1} }}'.format(alphabet[i], alp_numbers[n])
-    data['result'] = '{1} {0} ^ {{ {1} - 1 }}'.format(alphabet[i], alp_numbers[n])
-    dict_writer.writerow(data)
 
-    # derivation of expression of zero degree
-    data['expression'] = '{0} ^ {{ {1} }}'.format(alphabet[i], 0)
-    data['result'] = '0'
-    dict_writer.writerow(data)
+for row in dict_template_formulas:
+    for i in range(len_alphabet):
+        data['variable'] = alphabet[i]
+        n = random.randint(0, len_alp_Numb - 1)
+        n = n - 1 if n == i else n
 
+        data['expression'] = row['template_expression'].format(alphabet[i], alp_numbers[n])
+        data['result']     = row['template_result'].format(alphabet[i], alp_numbers[n])
+        dict_writer.writerow(data)
+
+del dict_writer
 
 ### latex to pdf to png for visualization of the result
 print(data['expression'])
@@ -42,5 +43,3 @@ line = 'test_rendering/test_rendering.png',\
        "\\frac {{ \partial }} {{ \partial {0} }} {1}  = {2}".format(data['variable'], data['expression'],data['result']), 'test.png', True
 utils.latex2png(line)
 
-
-del dict_writer
